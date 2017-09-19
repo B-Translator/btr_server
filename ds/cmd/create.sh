daf80e266c81fdf0b7144c9c070eca3ba3555503
Dashamir Hoxha <dashohoxha@gmail.com>
2017-09-19 21:57:12 +0200

    Mount drush cache on host.

--- ds/cmd/create.sh
+++ ds/cmd/create.sh
@@ -9,10 +9,11 @@ _EOF
 rename_function cmd_create orig_cmd_create
 cmd_create() {
     local code_dir=$(dirname $(realpath $APP_DIR))
-    mkdir -p var-www
+    mkdir -p var-www drush-cache
     orig_cmd_create \
         --mount type=bind,src=$code_dir,dst=/usr/local/src/btr_server \
         --mount type=bind,src=$(pwd)/var-www,dst=/var/www \
+        --mount type=bind,src=$(pwd)/drush-cache,dst=/root/.drush/cache \
         --workdir /var/www \
         --env CODE_DIR=/usr/local/src/btr_server \
         --env DRUPAL_DIR=/var/www/btr \
