11950b204265a33d6affddcb15df71a185c5ed51
Dashamir Hoxha <dashohoxha@gmail.com>
2018-03-29 16:09:31 +0200

    Improve backup and restore commands

--- ds/cmd/backup.sh
+++ ds/cmd/backup.sh
@@ -1,7 +1,9 @@
 cmd_backup_help() {
     cat <<_EOF
-    backup [<app>]
-        Make a backup of files, database, etc.
+    backup [full | diff | <app>]
+        Make a backup of files, databases, etc.
+        'full' makes a full backup of everything
+        'diff' makes only a differential backup
         <app> can be 'btr', 'btr_dev', etc.
 
 _EOF
@@ -9,18 +11,38 @@ _EOF
 
 cmd_backup() {
     set -x
+    local arg=${1:-full}
+    case $arg in
+        full)
+            _make_full_backup
+            ;;
+        diff)
+            _make_diff_backup
+            ;;
+        *)
+            _make_app_backup $arg
+            ;;
+    esac
+}
+
+_make_app_backup() {
     local app=${1:-btr}
 
     # create the backup dir
-    backup="backup-$app-$(date +%Y%m%d)"
+    local backup="backup-$app-$(date +%Y%m%d)"
     rm -rf $backup
     rm -f $backup.tgz
     mkdir $backup
 
-    # copy to the backup dir the database dump
+    # disable the site for maintenance
+    ds exec drush @$app vset maintenance_mode 1
+
+    # clear the cache
     ds exec drush @$app cache-clear all
+
+    # dump the content of the databases
     ds exec drush @$app sql-dump --ordered-dump \
-           --result-file=/host/$backup/database.sql
+           --result-file=/host/$backup/$app.sql
 
     # copy app files to the backup dir
     cp -a var-www/$app $backup
@@ -28,4 +50,44 @@ cmd_backup() {
     # make the backup archive
     tar --create --gzip --preserve-permissions --file=$backup.tgz $backup/
     rm -rf $backup/
+
+    # enable the site
+    ds exec drush @$app vset maintenance_mode 0
+}
+
+_make_full_backup() {
+    # create the backup dir
+    local backup="backup-full-$(date +%Y%m%d)"
+    rm -rf $backup
+    rm -f $backup.tgz
+    mkdir $backup
+
+    # disable the site for maintenance
+    ds exec drush --yes @local_btr vset maintenance_mode 1
+
+    # clear the cache
+    ds exec drush --yes @local_btr cache-clear all
+
+    # dump the content of the databases
+    ds exec drush @btr sql-dump --ordered-dump \
+       --result-file=/host/$backup/btr.sql
+    ds exec drush @btr sql-dump --ordered-dump \
+       --database=btr_db \
+       --result-file=/host/$backup/btr_data.sql
+    ds exec drush @btr_dev sql-dump --ordered-dump \
+       --result-file=/host/$backup/btr_dev.sql
+
+    # copy app files to the backup dir
+    cp -a var-www/{btr,btr_dev,downloads} $backup/
+
+    # make the backup archive
+    tar --create --gzip --preserve-permissions --file=$backup.tgz $backup/
+    rm -rf $backup/
+
+    # enable the site
+    ds exec drush --yes @local_btr vset maintenance_mode 0
+}
+
+_make_diff_backup() {
+    :
 }
--- ds/cmd/restore.sh
+++ ds/cmd/restore.sh
@@ -1,35 +1,95 @@
 cmd_restore_help() {
     cat <<_EOF
-    restore <backup-file.tgz> [<app>]
+    restore <backup-file.tgz>
         Restore application from the given backup file.
-        <app> can be 'btr', 'btr_dev', etc.
 
 _EOF
 }
 
 cmd_restore() {
-    set -x
     # get the backup file
     local file=$1
-    test -f "$file" || fail "Usage: $COMMAND <backup-file.tgz>"
-    local dir=${file%%.tgz}
-    [[ $file != $dir ]] || fail "Usage: $COMMAND <backup-file.tgz>"
+    test -f "$file" || fail "Usage:\n$(cmd_restore_help)"
+    [[ $file != ${file%%.tgz} ]] || fail "Usage:\n$(cmd_restore_help)"
+
+    # get the type of backup and
+    # call the appropriate restore function
+    local type=$(echo $file | cut -d- -f2)
+    set -x
+    case $type in
+        full)
+            _make_full_restore $file
+            ;;
+        diff)
+            _make_diff_restore $file
+            ;;
+        *)
+            _make_app_restore $file
+    esac
+}
+
+_make_app_restore() {
+    local file=$1
+    local app=$(echo $file | cut -d- -f2)
+    local backup=${file%%.tgz}
+    backup=$(basename $backup)
+
+    # disable the site for maintenance
+    ds exec drush @$app vset maintenance_mode 1
 
     # extract the backup archive
     tar --extract --gunzip --preserve-permissions --file=$file
-    dir=$(basename $dir)
 
     # restore the database
-    local app=${2:-btr}
     ds exec drush @$app sql-drop --yes
-    ds exec drush @$app sql-query --file=/host/$dir/database.sql
+    ds exec drush @$app sql-query \
+       --file=/host/$backup/$app.sql
 
     # restore application files
-    ds stop
     rm -rf var-www/$app
-    cp -a $dir/$app var-www/
-    ds start
+    cp -a $backup/$app var-www/
 
     # clean up
-    rm -rf $dir
+    rm -rf $backup
+
+    # enable the site
+    ds exec drush @$app vset maintenance_mode 0
+}
+
+_make_full_restore() {
+    local file=$1
+    local backup=${file%%.tgz}
+    backup=$(basename $backup)
+
+    # disable the site for maintenance
+    ds exec drush --yes @local_btr vset maintenance_mode 1
+
+    # extract the backup archive
+    tar --extract --gunzip --preserve-permissions --file=$file
+
+    # restore the content of the databases
+    ds exec drush @btr sql-drop --yes
+    ds exec drush @btr sql-query \
+       --file=/host/$backup/btr.sql
+    ds exec drush @btr_dev sql-drop --yes
+    ds exec drush @btr_dev sql-query \
+       --file=/host/$backup/btr_dev.sql
+    ds exec drush @btr sql-drop --database=btr_db --yes
+    ds exec drush @btr sql-query \
+       --database=btr_db \
+       --file=/host/$backup/btr_data.sql
+
+    # restore application files
+    rm -rf var-www/{btr,btr_dev,downloads}
+    cp -a $backup/{btr,btr_dev,downloads} var-www/
+
+    # clean up
+    rm -rf $backup
+
+    # enable the site
+    ds exec drush --yes @local_btr vset maintenance_mode 0
+}
+
+_make_diff_restore() {
+    :
 }
